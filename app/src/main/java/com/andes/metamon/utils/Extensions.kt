3ae2c4f7658573f5131198258608a55a89c82b64
wonseok <dnjstjr245@gmail.com>
2023-01-28 00:31:09 +0900

    [feat] 확장 함수 등 기타 유틸 클래스 추가

---
+++ app/src/main/java/com/andes/metamon/utils/Extensions.kt
@@ -0,0 +1,70 @@
+package com.andes.metamon.utils
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.annotation.IdRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.os.bundleOf
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
+import androidx.lifecycle.viewModelScope
+import com.andes.metamon.R
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.realworld.android.logging.Logger
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+fun ImageView.setImage(url: String) {
+    Glide.with(this.context)
+        .load(url.ifEmpty { null })
+        .error(R.drawable.img_placeholder)
+        .centerCrop()
+        .transition(DrawableTransitionOptions.withCrossFade())
+        .into(this)
+}
+
+inline fun CoroutineScope.createExceptionHandler(
+    message: String,
+    crossinline action: (throwable: Throwable) -> Unit
+) = CoroutineExceptionHandler { _, throwable ->
+    Logger.e(throwable, message)
+    throwable.printStackTrace()
+
+    /**
+     * A [CoroutineExceptionHandler] can be called from any thread. So, if [action] is supposed to
+     * run in the main thread, you need to be careful and call this function on the a scope that
+     * runs in the main thread, such as a [viewModelScope].
+     */
+    launch {
+        action(throwable)
+    }
+}
+
+inline fun <reified T : Fragment> AppCompatActivity.replace(@IdRes frameId: Int) {
+    supportFragmentManager.commit {
+        replace<T>(frameId)
+        setReorderingAllowed(true)
+    }
+}
+
+fun Context.shortToast(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+}
+
+inline fun <reified T : Activity> Context.buildIntent(
+    vararg argument: Pair<String, Any?>
+) = Intent(this, T::class.java).apply {
+    putExtras(bundleOf(*argument))
+}
+
+inline fun <reified T : Activity> Context.navigateActivity(
+    vararg argument: Pair<String, Any?>
+) {
+    startActivity(buildIntent<T>(*argument))
+}
